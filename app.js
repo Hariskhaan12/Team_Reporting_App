e8e961cd575f1206f244b252f4bce760c60ce327
Hariskhaan12 <hariskhaan12@gmail.com>
2021-09-09 23:53:34 +0500

    Update (9/92021)
    authentication with local storage.

--- app.js
+++ app.js
@@ -1,78 +1,90 @@
-let flag = false;
-let flag2 = false;
+auth=firebase.auth();
+database=firebase.database();
 const SignUp = () => {
-    location.reload();
+    
+    // let flag = false;
+    // let flag2 = false;
+    // location.reload();
     let new_data = {
         name: document.getElementById("name").value,
         email: document.getElementById("em").value,
         pass: document.getElementById("password").value
     };
+    // function to check the any data field is empty or  null
     let check_object = (new_data) => {
         for (const key in new_data) {
             if (new_data[key] == "" || new_data[key] == null) {
                 flag2 = true
             }
-
-
-        }
-
-    }
-    check_object(new_data);
-    if (flag2 == false) {
-        let prev_data = localStorage.getItem("UserData");
-
-        if (prev_data == null) {
-            Users = [];
-
-        }
-        else {
-            Users = JSON.parse(prev_data);
-            let ch = JSON.parse(localStorage.getItem("UserData"));
-            ch.forEach(element => {
-                if (new_data.name == element.name) {
-
-                    flag = true;
-                }
-            });
-        }
-        if (flag == false) {
-            Users.push(new_data);
-            localStorage.setItem("UserData", JSON.stringify(Users));
-        }
-
-        else if (flag == true) {
-            alert("userName Already Taken");
-
-        }
-        else {
-            alert("data is incomplete")
         }
     }
-    else if (flag2 == true) {
-        alert("All Fields must be filled")
-    }
+
+
+    // check_object(new_data);
+    // if (flag2 == false) {
+    //     let prev_data = localStorage.getItem("UserData");
+
+    //     if (prev_data == null) {
+    //         Users = [];
+
+    //     }
+    //     else {
+    //         Users = JSON.parse(prev_data);
+    //         // Now to check weather the name is already taken or not
+    //         let ch = JSON.parse(localStorage.getItem("UserData"));
+    //         ch.forEach(element => {
+    //             if (new_data.name == element.name) {
+
+    //                 flag = true;
+    //             }
+    //         });
+    //     }
+    //     if (flag == false) {
+    //         Users.push(new_data);
+    //         localStorage.setItem("UserData", JSON.stringify(Users));
+    //     }
+
+    //     else if (flag == true) {
+    //         alert("userName Already Taken");
+
+    //     }
+    //     else {
+    //         alert("data is incomplete")
+    //     }
+    // }
+    // else if (flag2 == true) {
+    //     alert("All Fields must be filled")
+    // }
 }
 // login fucntionality
 
+document.getElementById("login").addEventListener('submit',(event)=>{
+    event.preventDefault();
+})
 const login = () => {
-    let loginflag=false;
+    let loginflag = false;
     let loginEmail = document.getElementById("loginEmail").value;
     let loginPass = document.getElementById("loginPass").value;
+
     let Record = JSON.parse(localStorage.getItem("UserData"));
 
     for (var i = 0; i < Record.length; i++) {
         if (Record[i].email == loginEmail && Record[i].pass == loginPass) {
-            loginflag=true;
+            loginflag = true;
+        
         }
-    }
-    if(loginflag==true)
-    {
-        console.log("congrats");
-    }
-    else{
-        console.log("Sorry User not Registered.!")
-    }
 
+    }
+    if (loginflag == true) {
+        alert("logged in ")
+       window.location.replace("Teams.html")
+    }
+    else if (loginPass == "" || loginEmail == "") {
+        alert("All fields must be filled")
+    }
+    else if (loginflag == false) {
+        alert("Email/Pass not matched")
+    }
 }
 
 
---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAz_MTtq4KGs6AQQa1nXz_UHXIsp7lefsk",
+    authDomain: "teamreportapp.firebaseapp.com",
+    projectId: "teamreportapp",
+    storageBucket: "teamreportapp.appspot.com",
+    messagingSenderId: "617116785035",
+    appId: "1:617116785035:web:1339c7420ce0a30739f78e",
+    measurementId: "G-HDZ4V9RE22"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
